fc42e05d6c0539289d1a13c038572c019a4f960e
Dmytro Shvaika <dima@Dmytros-MacBook-Pro.local>
2019-09-13 23:32:34 +0300

    wip

--- apollo/src/resolver.js
+++ apollo/src/resolver.js
@@ -5,14 +5,15 @@ import { logger } from './lib/index.js'
 
 import { inspect } from 'util'
 
+
+
+import { typeDefs } from './schema.js'
+
+
 const { Types } = mongoose
 const { ObjectId } = Types
 
-// const getData = (parent, field) => parent ? parent[field]
-
 const getFilterFromParent = (parent, type) => {
-  console.log(333, parent)
-  
   let filter = parent[type]
 
   if (Array.isArray(filter)) {
@@ -44,32 +45,56 @@ const getFilterFromArgs = (args) => {
   return filter
 }
 
+
+const makeData = (data, returnType) => {
+  const fields = typeDefs.output.find(t => t.name === returnType.toString()).fields
+
+  
+
+  Object.keys(fields).forEach(key => {
+    const type = fields[key].replace(/[[|\]|!]/g, '')
+    console.log('FIELD TYPE', type)
+    
+    if (!(key in data) && type !== 'Int' && type !== 'Boolean' && type !== 'String') {
+      delete data[key]
+    }
+  })
+
+  return data
+}
+
 const resolver = async (parent, args, context, info) => {
   const operation = info.operation.operation 
   const { returnType, fieldName, parentType } = info
   const collection = returnType.toString().replace(/[[|\]|!]/g, '')
-  const parentCollection = parentType.toString().replace(/[[|\]|!]/g, '')
+  // const parentCollection = parentType.toString().replace(/[[|\]|!]/g, '')
   let outputMessage
 
   context.args = Object.keys(args).length ? args : context.args 
 
+ 
   
   console.log('\n\n###################################')
   // console.log(inspect(info.fieldNodes[0].selectionSet.selections, {depth:5, colors:true}))  
-  // console.log(inspect(info.fieldNodes[0].selectionSet.selections, {depth:5, colors:true}))  
+  // console.log(inspect(info, {depth:10, colors:true}))  
   // console.log('COLLECTION:', collection)
   // console.log('ARGS:', args)
-  // console.log('RETURN_TYPE:', returnType)
+  console.log('RETURN_TYPE:', returnType)
   // console.log('FIELD_NAME:', fieldName)
   // console.log(info.fieldNodes[0].selectionSet.selections)
   // console.log('FILTER:', filter)
   // console.log('OPERATION:', operation)
   // console.log('PARENT:', parent)
-  // console.log('PARENT_TYPE:', parentCollection)
+  // console.log('PARENT_TYPE:', parentType)
   // console.log('CONTEXT:', context)
   
+  
+  
+
+
   switch(operation) {
     case 'query':
+      // eslint-disable-next-line
       const filter = parent ? getFilterFromParent(parent, fieldName) : 
         operation === 'query' ? getFilterFromArgs(args) : {}
 
@@ -82,73 +107,77 @@ const resolver = async (parent, args, context, info) => {
       }
       break
     case 'mutation':
-      if (context.args.input && context.args.input.id && Object.keys(context.args.input).length === 1) {
-        console.log('DELETE')
-      } else {
+      if (context.args.input && !context.args.input.id && Object.keys(context.args.input).length) {
+        console.log('CREATE')
+
+
+        // console.log('ARGS:', args)
         
-        if (context.args.input && !context.args.input.id && Object.keys(context.args.input).length) {
-          console.log('CREATE')
+        const data = makeData(context.args.input, returnType)
+        console.log('DATA:', data)
       
-          let data = JSON.parse(JSON.stringify(context.args.input))
+        outputMessage = await mongoose.connection.db.collection(collection).insertMany(data)
+        outputMessage = outputMessage.ops
 
-          if (parentType !== 'Mutation' && parent) {
-            data = data[fieldName]
-          }
+        // let data = JSON.parse(JSON.stringify(context.args.input))
 
-          if (Array.isArray(data)) {
-            data = data.map(d =>{
-              Object.keys(d).forEach(k => {
-                if (info.fieldNodes[0].selectionSet.selections.find(f => f.name.value === k && f.selectionSet)) {
-                  delete d[k]
-                }
-              })
-              return d
-            })
-          } else {
-            Object.keys(data).forEach(k => {
-              if (info.fieldNodes[0].selectionSet.selections.find(f => f.name.value === k && f.selectionSet)) {
-                delete data[k]
-              }
-            })
-          }
+        // if (parentType !== 'Mutation' && parent) {
+        //   data = data[fieldName]
+        // }
 
-          if (Array.isArray(data)) {
-            outputMessage = await mongoose.connection.db.collection(collection).insertMany(data)
-            outputMessage = outputMessage.ops
-          } else {
-            outputMessage = await mongoose.connection.db.collection(collection).insertOne(data)
-            outputMessage = outputMessage.ops[0]
-          }
+        // if (Array.isArray(data)) {
+        //   data = data.map(d =>{
+        //     Object.keys(d).forEach(k => {
+        //       if (info.fieldNodes[0].selectionSet.selections.find(f => f.name.value === k && f.selectionSet)) {
+        //         delete d[k]
+        //       }
+        //     })
+        //     return d
+        //   })
+        // } else {
+        //   Object.keys(data).forEach(k => {
+        //     if (info.fieldNodes[0].selectionSet.selections.find(f => f.name.value === k && f.selectionSet)) {
+        //       delete data[k]
+        //     }
+        //   })
+        // }
 
-          if (parent && parentCollection !== 'Mutation') {
-            await mongoose.connection.db.collection(parentCollection).update({
-              _id: ObjectId(context.parent.id)
-            }, {
-              $push: {
-                [fieldName]: {
-                  $each: isArray(returnType) ? outputMessage.map(o => o._id) : outputMessage._id
-                }
-              }
-            })
+        // if (Array.isArray(data)) {
+        // outputMessage = await mongoose.connection.db.collection(collection).insertMany(data)
+        // outputMessage = outputMessage.ops
+        // } else {
+        //   outputMessage = await mongoose.connection.db.collection(collection).insertOne(data)
+        //   outputMessage = outputMessage.ops[0]
+        // }
 
-            await mongoose.connection.db.collection(collection).updateMany({
-              _id: {
-                $in: Array.isArray(outputMessage) ? outputMessage.map(k => k._id) : [ outputMessage._id ]
-              }
-            }, {
-              $set: { [parentCollection.toLowerCase()]: ObjectId(context.parent.id) }
-            })
-          }
+        // if (parent && parentCollection !== 'Mutation') {
+        //   await mongoose.connection.db.collection(parentCollection).update({
+        //     _id: ObjectId(context.parent.id)
+        //   }, {
+        //     $push: {
+        //       [fieldName]: {
+        //         $each: isArray(returnType) ? outputMessage.map(o => o._id) : outputMessage._id
+        //       }
+        //     }
+        //   })
 
-          outputMessage = Array.isArray(outputMessage)
-            ? outputMessage.map(p => ({ ...p, id: p._id.toString() }))
-            : { ...outputMessage, id: outputMessage._id.toString() }
+        //   await mongoose.connection.db.collection(collection).updateMany({
+        //     _id: {
+        //       $in: Array.isArray(outputMessage) ? outputMessage.map(k => k._id) : [ outputMessage._id ]
+        //     }
+        //   }, {
+        //     $set: { [parentCollection.toLowerCase()]: ObjectId(context.parent.id) }
+        //   })
+        // }
 
-          context.parent = outputMessage
-        } else {
-          if (context.args.input && context.args.input.id && Object.keys(context.args.input).length > 1) {
-            console.log('UPDATE')
-          }
+        // outputMessage = Array.isArray(outputMessage)
+        //   ? outputMessage.map(p => ({ ...p, id: p._id.toString() }))
+        //   : { ...outputMessage, id: outputMessage._id.toString() }
+
+        // context.parent = outputMessage
+      } else {
+        if (context.args.input && context.args.input.id && Object.keys(context.args.input).length > 1) {
+          console.log('UPDATE')
         }
       }
       break
--- apollo/src/schema.js
+++ apollo/src/schema.js
@@ -4,6 +4,8 @@ import { fileURLToPath } from 'url'
 import graphql from 'graphql'
 import graphqlTools from 'graphql-tools'
 
+import { inspect } from 'util'
+
 const { makeExecutableSchema } = graphqlTools
 const { graphqlSync, introspectionQuery } = graphql
 
@@ -15,18 +17,66 @@ const __dirname = path.dirname(__filename);
 
 const schemaPath = path.join(__dirname, '../', 'schema')
 
-const types = directory => fs.readdirSync(directory)
+const getTypes = directory => fs.readdirSync(directory)
   .filter(file => /.*\.graphql/gi.test(file))
   .map(file => path.join(directory, file))
   .reduce((xs, file) => [...xs, fs.readFileSync(file, 'utf8')] , [])
 
+const types = getTypes(schemaPath)
 
+const typesDefs = graphqlSync(
+  makeExecutableSchema({ typeDefs: types}),
+  introspectionQuery
+)
 
-// const schema = mergeSchemas({
-//   schemas: types(schemaPath),
-// });
+const processFields = (fields) => {
+  const output = {}
 
-// export default schema
-// console.log(111, types);
+  fields.forEach(field => {
+    let type
 
-export default [ ...types(schemaPath) ]
+    if (field.type.kind === 'LIST' && field.type.ofType && !field.type.ofType !== null) {
+      type = `[${field.type.ofType.name}]`
+    }
+    if (field.type.kind === 'NON_NULL' && field.type.ofType && !field.type.ofType !== null) {
+      type = field.type.ofType.name
+    }
+
+    type = type || field.type.name
+    
+    // if (field.name === 'valueToChangeOn'){
+    //   console.log(field.name, type, inspect(field.type, {depth:6, colors:true}))
+    // }
+    
+
+    output[field.name] = type
+  })
+
+  return output
+}
+
+const filterTypes = (graphqlType, allTypes) => {
+  const filteredTypes = allTypes.data.__schema.types.filter(t => t.kind === graphqlType)
+
+  return filteredTypes.map(f => {
+    switch (f.kind) {
+      case 'INPUT_OBJECT':
+        return {
+          name: f.name,
+          fields: processFields(f.inputFields)
+        }
+      case 'OBJECT':
+        return {
+          name: f.name,
+          fields: processFields(f.fields)
+        }
+    }
+  })
+}
+
+export const typeDefs = {
+  input: filterTypes('INPUT_OBJECT', typesDefs),
+  output: filterTypes('OBJECT', typesDefs)
+}
+
+export default [ ...types]
