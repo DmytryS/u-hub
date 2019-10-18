27d75c00b6c6d676b507f3a6340a41c9864931ea
Dmytro Shvaika <shvajka@gmail.com>
2019-10-19 01:22:35 +0300

    wip

--- apollo/src/listener.js
+++ apollo/src/listener.js
@@ -1,5 +1,38 @@
 import { inspect } from 'util'
+import mongodb from 'mongodb'
 import { logger, mongo } from './lib/index.js'
+
+const { ObjectId } = mongodb
+
+const getDevice =  (client, message) => client.collection('Device').findOneAndUpdate(
+  { name: message.input.device.name },
+  {
+    $set: { name: message.input.device.name }
+  },
+  {
+    upsert: true,
+    returnNewDocument: true,
+    returnOriginal: false,
+  }
+).then(res => res.value)
+
+const getSensor = (client, message, device) => client.collection('Sensor').findOneAndUpdate(
+  {
+    type: message.input.device.sensor.type,
+    device: device._id
+  },
+  {
+    $set: {
+      type: message.input.device.sensor.type,
+      device: device._id
+    }
+  },
+  {
+    upsert: true,
+    returnNewDocument: true,
+    returnOriginal: false,
+  }
+).then(res => res.value)
 
 export default async (message) => {
   if ( Object.keys(message).length === 0 || message.info.operation === 'set-value') {
@@ -10,67 +43,56 @@ export default async (message) => {
 
 
   const client = mongo.connection()
-  const device = await client.collection('Device').findOneAndUpdate(
-    { name: message.input.device.name },
-    {
-      $set: { name: message.input.device.name }
-    },
-    {
-      upsert: true,
-      returnNewDocument: true,
-      returnOriginal: false,
-    }
-  ).then(res => res.value)
+  let device
+  let sensor
 
-  const sensor = await client.collection('Sensor').findOneAndUpdate(
-    {
-      type: message.input.device.sensor.type,
-      device: device._id
-    },
-    {
-      $set: {
-        type: message.input.device.sensor.type,
-        device: device._id
-      }
-    },
-    {
-      upsert: true,
-      returnNewDocument: true,
-      returnOriginal: false,
-    }
-  ).then(res => res.value)
-
-  if (!device.sensors || device.sensors.length === 0 || !device.sensors.find(s => s.toString() === sensor._id.toString())) {
-    await client.collection('Device').updateOne(
-      {
-        _id: device._id
-      },
-      {
-        $push: {
-          sensors: {
-            $each: [sensor._id]
+  if (['add-value', 'get-automatic-actions'].includes(message.info.operation)) {
+    device = await getDevice(client, message)
+    sensor = await getSensor(client, message, device)
+  
+    if (!device.sensors || device.sensors.length === 0 || !device.sensors.find(s => s.toString() === sensor._id.toString())) {
+      await client.collection('Device').updateOne(
+        {
+          _id: device._id
+        },
+        {
+          $push: {
+            sensors: {
+              $each: [sensor._id]
+            }
           }
         }
-      }
-    )
+      )
+    }
   }
+
 
   // logger.info(`DEVICE: ${inspect(device, { depth:2, colors: true})}`)
   // logger.info(`SENSOR: ${inspect(sensor, { depth:2, colors: true})}`)
 
   switch(message.info.operation) {
     case 'add-value':
+      // // eslint-disable-next-line
+      // const device = await getDevice(client, message)
+      // // eslint-disable-next-line
+      // const sensor = await getSensor(client, message, device)
+  
       await client.collection('Value').insertOne(
         {
           sensor: device._id,
           value: message.input.device.sensor.value
-        })
-        
+        }
+      )
       break
     case 'get-automatic-actions':
+      // // eslint-disable-next-line
+      // const _device = await getDevice(client, message)
+      // // eslint-disable-next-line
+      // const _sensor = await getSensor(client, message, _device)
       // eslint-disable-next-line
       const automaticActions = await client.collection('AutomaticAction').find({
-        sensor: sensor._id
+        sensor: sensor._id,
+        enabled: true
       }).toArray()
 
       // eslint-disable-next-line
@@ -87,11 +109,29 @@ export default async (message) => {
     case 'get-actions':
       // eslint-disable-next-line
       const actions = await client.collection('Action').find({
-        _id: { $in: message.input.action }
+        _id: { $in: message.input.action.map(a => ObjectId(a)) }
       }).toArray()
       
       // eslint-disable-next-line
       message.output = actions
+      break
+    case 'get-device':
+      // eslint-disable-next-line
+      const _device = await client.collection('Device').find({
+        _id: Array.isArray(message.input.device) ? { $in: message.input.device.map(d => ObjectId(d)) } : message.input.device
+      }).toArray()
+          
+      // eslint-disable-next-line
+      message.output = _device
+      break
+    case 'get-sensor':
+      // eslint-disable-next-line
+      const _sensor = await client.collection('Sensor').find({
+        _id: Array.isArray(message.input.sensor) ? { $in: message.input.sensor.map(s => ObjectId(s)) } : message.input.sensor
+      }).toArray()
+
+      // eslint-disable-next-line
+      message.output = _sensor
       break
     default:
       logger.error(`Unknown operation ${message.info.operation}`)
--- automatic-action/src/index.js
+++ automatic-action/src/index.js
@@ -1,16 +1,19 @@
 import 'dotenv/config.js'
 import { logger, amqp } from './lib/index.js'
+import { inspect } from 'util'
 
 const {
   AMQP_APOLLO_QUEUE,
   AMQP_AUTOMATIC_ACTION_QUEUE,
-  // AMQP_MQTT_LISTENER_QUEUE
+  AMQP_MQTT_LISTENER_QUEUE
 } = process.env
 
 const listener = async (message) => {
-  logger.info(`MESSAGE: ${message}`)
+  logger.info(`MESSAGE: ${inspect(message, {depth: 7, colors: true})}`)
 
-  const automaticActions = await amqp.request(
+  const currentValue = message.input.device.sensor.value
+
+  const { output: automaticActions } = await amqp.request(
     AMQP_APOLLO_QUEUE,
     {
       info: {
@@ -27,7 +30,94 @@ const listener = async (message) => {
     }
   )
 
-  console.log('AUTOMATIC_ACTIONS:', automaticActions)
+  const actionIds = automaticActions
+    .flatMap(automaticAction => {
+      let actions
+
+      switch(automaticAction.condition){
+        case 'LESS':
+          actions = automaticAction.valueToCompare < currentValue ? automaticAction.actions: [false]
+          break
+        case 'MORE':
+          actions = automaticAction.valueToCompare > currentValue ? automaticAction.actions: [false]
+          break
+        case 'MORE_OR_EQUAL':
+          actions = automaticAction.valueToCompare >= currentValue ? automaticAction.actions: [false]
+          break
+        case 'LESS_OR_EQUAL':
+          actions = automaticAction.valueToCompare <= currentValue ? automaticAction.actions: [false]
+          break
+        case 'NOT_EQUAL':
+          actions = automaticAction.valueToCompare !== currentValue ? automaticAction.actions: [false]
+          break
+        case 'EQUAL':
+          actions = automaticAction.valueToCompare === currentValue ? automaticAction.actions: [false]
+          break
+      }
+
+      return actions
+    })
+    .filter(automaticAction => !!automaticAction)
+
+  // console.log('AUTOMATIC_ACTIONS:', inspect(automaticActions,{depth:7, colors: true}))
+
+  const { output: actions } = await amqp.request(
+    AMQP_APOLLO_QUEUE,
+    {
+      info: {
+        operation: 'get-actions'
+      },
+      input: {
+        action: actionIds
+      }
+    }
+  )
+
+  // console.log('ACTIONS:', inspect(actions,{depth:7, colors: true}))
+
+  const { output: sensors } = await amqp.request(
+    AMQP_APOLLO_QUEUE,
+    {
+      info: {
+        operation: 'get-sensor'
+      },
+      input: {
+        sensor: actions.map(a => a.sensor)
+      }
+    }
+  )
+  
+  const { output: devices } = await amqp.request(
+    AMQP_APOLLO_QUEUE,
+    {
+      info: {
+        operation: 'get-device'
+      },
+      input: {
+        device: sensors.map(s => s.device)
+      }
+    }
+  )
+  
+  // console.log('DEVICES:', inspect(devices,{depth:7, colors: true}))
+
+  await Promise.all(sensors.map(s => amqp.request(
+    AMQP_MQTT_LISTENER_QUEUE,
+    {
+      info: {
+        operation: 'set-value'
+      },
+      input: {
+        device: {
+          name: devices.find(d => d._id === s.device).name,
+          sensor: {
+            type: s.type,
+            value: actions.find(a => a.sensor === s._id).valueToChangeOn
+          }
+        }
+      }
+    }
+  )))
     
 }
 
